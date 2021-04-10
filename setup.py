4f120934df4ccaf4a3559c690b1429922e0938fd
Robert Farmer <r.j.farmer@uva.nl>
2021-04-10 12:44:16 +0200

    Add readme

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(name='pyAstroRef',
       url='https://github.com/rjfarmer/pyAstroRef',
       packages=find_packages(),
       scripts=["bin/pyastroref"],
-      data_files=[('icons',['icons/Feed-icon.png']),('icons',['icons/Generic_Feed-icon.svg'])],
       classifiers=[
 			"Development Status :: 1 - Planning",
 			"Intended Audience :: Science/Research",
