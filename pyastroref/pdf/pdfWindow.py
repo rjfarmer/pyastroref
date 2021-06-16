7f5d8186ea1f79ec14dba8539a90c69eefcc5ba2
Robert Farmer <r.j.farmer@uva.nl>
2021-06-16 17:03:46 +0200

    Add pdf search

--- pyastroref/pdf/pdfWindow.py
+++ pyastroref/pdf/pdfWindow.py
@@ -3,6 +3,7 @@ import sys
 import threading
 import tempfile
 import shutil
+import time
 
 import gi
 gi.require_version("Gtk", "3.0")
@@ -41,8 +42,9 @@ class _Pdf(object):
 
         self.load_pdf()
 
-        # Autosave pdf every 1 minutes (set in milliseconds)
-        #GLib.timeout_add(1 * 60000 ,self.save)
+        self.highlight_color = Gdk.RGBA()
+        self.highlight_color.parse('light yellow')
+
         # Autosave pdf every 5 minutes (set in milliseconds)
         GLib.timeout_add(5 * 60000 ,self.save)
 
@@ -76,22 +78,25 @@ class _Pdf(object):
         cur_page = self.model.get_page()
         self.model.set_page(cur_page-1)
 
+    def set_page(self, page):
+        n_pages = self.pdf.get_n_pages()
+        if page >= n_pages:
+            page = n_pages - 1
+        elif page < 0:
+            page = 0
+        self.model.set_page(page)
+
+
     def highlighted_text(self, *args):
         return self.view.get_selected_text()
 
     def rotate_left(self, *args):
         rotation = self.model.get_rotation()
-
-        rotation -= 90
-
-        self.model.set_rotation(rotation)
+        self.model.set_rotation(rotation-90)
 
     def rotate_right(self, *args):
         rotation = self.model.get_rotation()
-
-        rotation += 90
-
-        self.model.set_rotation(rotation)
+        self.model.set_rotation(rotation+90)
 
     def save(self):
         tmp_file = tempfile.NamedTemporaryFile(suffix='.pdf',delete=False)
@@ -152,6 +157,7 @@ class _Pdf(object):
     def copy_text(self):
         text = self.highlighted_text().replace('\n',' ')
         utils.clipboard(text)
+
     def print(self):
         print_op = Gtk.PrintOperation()
         print_op.connect("begin-print", self.begin_print)
@@ -181,13 +187,21 @@ class _Pdf(object):
     def highlight_text(self):
         self.view.add_text_markup_annotation_for_selected_text()
 
-
     def save_as(self):
         utils.save_as(self._filename, self.save_with_filename)
 
     def save_with_filename(self, filename):
         self._filename = filename
         self.save()
+
+    def search(self, text):
+        self._find_job = EvinceView.JobFind.new(
+            document=self.pdf, start_page=0, n_pages=self.pdf.get_n_pages(),
+            text=text, case_sensitive=False
+        )
+        self.view.find_started(self._find_job)
+        EvinceView.Job.scheduler_push_job(
+            self._find_job, EvinceView.JobPriority.PRIORITY_NONE)
 
 
 class SearchBar(Gtk.HBox):
@@ -213,7 +227,10 @@ class SearchBar(Gtk.HBox):
 
         self.pack_start(hb,True,True,0)
 
-        self.sb.connect("changed", self.search)
+        self.sb.connect("search-changed", self.search)
+        self.sb.connect("stop-search", self.search_stop)
+        self.sb.connect("previous-match", self.on_prev)
+        self.sb.connect("next-match", self.on_next)
 
 
     def add_button(self,button):
@@ -231,13 +248,14 @@ class SearchBar(Gtk.HBox):
     def on_prev(self, button):
         self.pdf.view.find_previous()
 
-
     def search(self, widget):
         query = widget.get_text().lower()
         print(query)
+        self.pdf.search(query)
+        self.pdf.view.find_next()
 
-        search = EvinceView.JobFind()
-        self.pdf.view.find_started(search)
+    def search_stop(self, widget):
+        self.pdf.view.find_cancel()
 
 
 class pdfHead(Gtk.HBox):
@@ -246,10 +264,17 @@ class pdfHead(Gtk.HBox):
         self.pdf = pdf
 
         buttons = [
-            {'image':'applications-graphics','callback':None,'tooltip':'Highlight text','button':None},
-            {'image':'font-x-generic','callback':None,'tooltip':'Add annotation','button':None}
+            {'image':'list-add','callback':None,'tooltip':'Add annotation','button':None}
         ]
         
+        col = Gdk.RGBA()
+        col.parse('light yellow')
+        self.cb = Gtk.ColorButton.new_with_rgba(col)
+        self.pack_start(self.cb,False,False,0)
+        self.cb.props.title = 'Highlight text color'
+        self.cb.set_tooltip_text('Highlight text color')
+        self.cb.connect('color_set',self.on_cb_set)
+
         for i in buttons:
             self.add_button(i)
 
@@ -271,3 +296,6 @@ class pdfHead(Gtk.HBox):
             button['button'].connect('clicked',button['callback'])
         self.pack_start(button['button'],False,False,0)
         button['button'].set_tooltip_text(button['tooltip'])
+
+    def on_cb_set(self, widget):
+        self.pdf.highlight_color = widget.get_rgba()
